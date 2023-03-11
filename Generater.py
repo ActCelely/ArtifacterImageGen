4f3aa775c8b15c371ba9599f2664597fa3cb42fc
Celely <tomaranai.baburu@gmail.com>
2023-03-11 11:27:48 +0900

    autopep8にかけた

--- Generater.py
+++ Generater.py
@@ -60,7 +60,7 @@ def culculate_op(data: dict) -> list:
         for ctg, state in dup_state.items():
             possiblity = dup[ctg][str(state)]
             for p in possiblity:
-                if len(p) == long or len(p) == long-1:
+                if len(p) == long or len(p) == long - 1:
                     idx = keymap.index(ctg)
                     res[idx] = p
                     return res
@@ -116,7 +116,7 @@ def culculate_op(data: dict) -> list:
         p = itertools.product(p1, p2, p3)
         r = None
         for v in p:
-            if sum(v) == long or sum(v) == long-1:
+            if sum(v) == long or sum(v) == long - 1:
                 r = v
                 break
 
@@ -149,7 +149,7 @@ def culculate_op(data: dict) -> list:
         p = itertools.product(p1, p2, p3, p4)
         r = None
         for v in p:
-            if sum(v) == long or sum(v) == long-1:
+            if sum(v) == long or sum(v) == long - 1:
                 r = v
                 break
 
@@ -204,8 +204,6 @@ def generation(data: dict) -> str:
 
     Base = Image.open(f'{cwd}/Base/{element}.png')
 
-
-
     # キャラクター
     CharacterCostume = CharacterData.get('Costume')
     if CharacterName in ['蛍', '空']:
@@ -222,7 +220,7 @@ def generation(data: dict) -> str:
     Shadow = Image.open(f'{cwd}/Assets/shadow.png').resize(Base.size)
     CharacterImage = CharacterImage.crop((289, 0, 1728, 1024))
     CharacterImage = CharacterImage.resize(
-        (int(CharacterImage.width*0.75), int(CharacterImage.height*0.75)))
+        (int(CharacterImage.width * 0.75), int(CharacterImage.height * 0.75)))
 
     CharacterAvatarMask = CharacterImage.copy()
 
@@ -240,9 +238,6 @@ def generation(data: dict) -> str:
     Base = Image.alpha_composite(Base, CharacterPaste)
     Base = Image.alpha_composite(Base, Shadow)
 
-
-
-
     # 武器
     Weapon = Image.open(
         f'{cwd}/weapon/{WeaponName}.png').convert("RGBA").resize((128, 128))
@@ -256,35 +251,31 @@ def generation(data: dict) -> str:
     WeaponRImage = Image.open(
         f'{cwd}/Assets/Rarelity/{WeaponRarelity}.png').convert("RGBA")
     WeaponRImage = WeaponRImage.resize(
-        (int(WeaponRImage.width*0.97), int(WeaponRImage.height*0.97)))
+        (int(WeaponRImage.width * 0.97), int(WeaponRImage.height * 0.97)))
     WeaponRPaste = Image.new("RGBA", Base.size, (255, 255, 255, 0))
     WeaponRMask = WeaponRImage.copy()
 
     WeaponRPaste.paste(WeaponRImage, (1422, 173), mask=WeaponRMask)
     Base = Image.alpha_composite(Base, WeaponRPaste)
 
-
-
     # 天賦
     TalentBase = Image.open(f'{cwd}/Assets/TalentBack.png')
     TalentBasePaste = Image.new("RGBA", Base.size, (255, 255, 255, 0))
     TalentBase = TalentBase.resize(
-        (int(TalentBase.width/1.5), int(TalentBase.height/1.5)))
+        (int(TalentBase.width / 1.5), int(TalentBase.height / 1.5)))
 
     for i, t in enumerate(['通常', 'スキル', "爆発"]):
         TalentPaste = Image.new("RGBA", TalentBase.size, (255, 255, 255, 0))
         Talent = Image.open(
             f'{cwd}/character/{CharacterName}/{t}.png').resize((50, 50)).convert('RGBA')
         TalentMask = Talent.copy()
-        TalentPaste.paste(Talent, (TalentPaste.width//2-25,
-                          TalentPaste.height//2-25), mask=TalentMask)
+        TalentPaste.paste(Talent, (TalentPaste.width // 2 - 25,
+                          TalentPaste.height // 2 - 25), mask=TalentMask)
 
         TalentObject = Image.alpha_composite(TalentBase, TalentPaste)
-        TalentBasePaste.paste(TalentObject, (15, 330+i*105))
+        TalentBasePaste.paste(TalentObject, (15, 330 + i * 105))
 
     Base = Image.alpha_composite(Base, TalentBasePaste)
-
-
 
     # 凸
     CBase = Image.open(
@@ -296,33 +287,37 @@ def generation(data: dict) -> str:
     CPaste = Image.new("RGBA", Base.size, (255, 255, 255, 0))
     for c in range(1, 7):
         if c > CharacterConstellations:
-            CPaste.paste(Clock, (666, -10+c*93), mask=ClockMask)
+            CPaste.paste(Clock, (666, -10 + c * 93), mask=ClockMask)
         else:
             CharaC = Image.open(
                 f'{cwd}/character/{CharacterName}/{c}.png').convert("RGBA").resize((45, 45))
             CharaCPaste = Image.new("RGBA", CBase.size, (255, 255, 255, 0))
             CharaCMask = CharaC.copy()
-            CharaCPaste.paste(CharaC, (int(CharaCPaste.width/2)-25,
-                              int(CharaCPaste.height/2)-23), mask=CharaCMask)
+            CharaCPaste.paste(CharaC,
+                              (int(CharaCPaste.width / 2) - 25,
+                               int(CharaCPaste.height / 2) - 23),
+                              mask=CharaCMask)
 
             Cobject = Image.alpha_composite(CBase, CharaCPaste)
-            CPaste.paste(Cobject, (666, -10+c*93))
+            CPaste.paste(Cobject, (666, -10 + c * 93))
 
     Base = Image.alpha_composite(Base, CPaste)
     D = ImageDraw.Draw(Base)
 
     D.text((30, 20), CharacterName, font=config_font(48))
-    levellength = D.textlength("Lv."+str(CharacterLevel), font=config_font(25))
+    levellength = D.textlength(
+        "Lv." + str(CharacterLevel),
+        font=config_font(25))
     friendshiplength = D.textlength(str(FriendShip), font=config_font(25))
-    D.text((35, 75), "Lv."+str(CharacterLevel), font=config_font(25))
-    D.rounded_rectangle((35+levellength+5, 74, 77+levellength +
+    D.text((35, 75), "Lv." + str(CharacterLevel), font=config_font(25))
+    D.rounded_rectangle((35 + levellength + 5, 74, 77 + levellength +
                         friendshiplength, 102), radius=2, fill="black")
     FriendShipIcon = Image.open(f'{cwd}/Assets/Love.png').convert("RGBA")
     FriendShipIcon = FriendShipIcon.resize(
-        (int(FriendShipIcon.width*(24/FriendShipIcon.height)), 24))
+        (int(FriendShipIcon.width * (24 / FriendShipIcon.height)), 24))
     Fmask = FriendShipIcon.copy()
-    Base.paste(FriendShipIcon, (42+int(levellength), 76), mask=Fmask)
-    D.text((73+levellength, 74), str(FriendShip), font=config_font(25))
+    Base.paste(FriendShipIcon, (42 + int(levellength), 76), mask=Fmask)
+    D.text((73 + levellength, 74), str(FriendShip), font=config_font(25))
 
     D.text((42, 397), f'Lv.{CharacterTalent["通常"]}', font=config_font(
         17), fill='aqua' if CharacterTalent["通常"] >= 10 else None)
@@ -335,19 +330,45 @@ def generation(data: dict) -> str:
         sumv = CharacterStatus[state]
         plusv = sumv - CharacterBase[state]
         basev = CharacterBase[state]
-        return f"+{format(plusv,',')}", f"{format(basev,',')}", D.textlength(f"+{format(plusv,',')}", font=config_font(12)), D.textlength(f"{format(basev,',')}", font=config_font(12))
+        return f"+{format(plusv,',')}", f"{format(basev,',')}", D.textlength(f"+{format(plusv,',')}",
+                                                                             font=config_font(12)), D.textlength(f"{format(basev,',')}", font=config_font(12))
 
-    disper = ['会心率', '会心ダメージ', '攻撃パーセンテージ', '防御パーセンテージ', 'HPパーセンテージ', '水元素ダメージ', '物理ダメージ', '風元素ダメージ',
-              '岩元素ダメージ', '炎元素ダメージ', '与える治癒効果', '与える治療効果', '雷元素ダメージ', '氷元素ダメージ', '草元素ダメージ', '与える治癒効果', '元素チャージ効率']
+    disper = [
+        '会心率',
+        '会心ダメージ',
+        '攻撃パーセンテージ',
+        '防御パーセンテージ',
+        'HPパーセンテージ',
+        '水元素ダメージ',
+        '物理ダメージ',
+        '風元素ダメージ',
+        '岩元素ダメージ',
+        '炎元素ダメージ',
+        '与える治癒効果',
+        '与える治療効果',
+        '雷元素ダメージ',
+        '氷元素ダメージ',
+        '草元素ダメージ',
+        '与える治癒効果',
+        '元素チャージ効率']
     StateOP = ('HP', '攻撃力', "防御力", "元素熟知", "会心率", "会心ダメージ", "元素チャージ効率")
     for k, v in CharacterStatus.items():
-        if k in ['氷元素ダメージ', '水元素ダメージ', '岩元素ダメージ', '草元素ダメージ', '風元素ダメージ', '炎元素ダメージ', '物理ダメージ', '与える治癒効果', '雷元素ダメージ'] and v == 0:
+        if k in [
+            '氷元素ダメージ',
+            '水元素ダメージ',
+            '岩元素ダメージ',
+            '草元素ダメージ',
+            '風元素ダメージ',
+            '炎元素ダメージ',
+            '物理ダメージ',
+            '与える治癒効果',
+                '雷元素ダメージ'] and v == 0:
             k = f'{element}元素ダメージ'
         try:
             i = StateOP.index(k)
-        except:
+        except BaseException:
             i = 7
-            D.text((844, 67 + i*70), k, font=config_font(26))
+            D.text((844, 67 + i * 70), k, font=config_font(26))
             opicon = Image.open(f'{cwd}/emotes/{k}.png').resize((40, 40))
             oppaste = Image.new('RGBA', Base.size, (255, 255, 255, 0))
 
@@ -357,7 +378,7 @@ def generation(data: dict) -> str:
 
         if k not in disper:
             statelen = D.textlength(format(v, ","), config_font(26))
-            D.text((1360-statelen, 67+i*70),
+            D.text((1360 - statelen, 67 + i * 70),
                    format(v, ","), font=config_font(26))
 
         else:
@@ -367,14 +388,14 @@ def generation(data: dict) -> str:
 
         if k in ['HP', '防御力', '攻撃力']:
             HPpls, HPbase, HPsize, HPbsize = genbasetext(k)
-            D.text((1360-HPsize, 97+i*70), HPpls,
+            D.text((1360 - HPsize, 97 + i * 70), HPpls,
                    fill=(0, 255, 0, 180), font=config_font(12))
-            D.text((1360-HPsize-HPbsize-1, 97+i*70), HPbase,
+            D.text((1360 - HPsize - HPbsize - 1, 97 + i * 70), HPbase,
                    font=config_font(12), fill=(255, 255, 255, 180))
 
     D.text((1582, 47), WeaponName, font=config_font(26))
     wlebellen = D.textlength(f'Lv.{WeaponLevel}', font=config_font(24))
-    D.rounded_rectangle((1582, 80, 1582+wlebellen+4, 108),
+    D.rounded_rectangle((1582, 80, 1582 + wlebellen + 4, 108),
                         radius=1, fill='black')
     D.text((1584, 82), f'Lv.{WeaponLevel}', font=config_font(24))
 
@@ -389,21 +410,25 @@ def generation(data: dict) -> str:
         "元素チャージ効率": "元チャ効率",
         "HPパーセンテージ": "HP%",
     }
-    if WeaponSubOPKey != None:
+    if WeaponSubOPKey is not None:
         BaseAtk = Image.open(
             f'{cwd}/emotes/{WeaponSubOPKey}.png').resize((23, 23))
         BaseAtkmask = BaseAtk.copy()
         Base.paste(BaseAtk, (1600, 155), mask=BaseAtkmask)
 
-        D.text((1623, 155), f'{optionmap.get(WeaponSubOPKey) or WeaponSubOPKey}  {str(WeaponSubOPValue)+"%" if WeaponSubOPKey in disper else format(WeaponSubOPValue,",")}', font=config_font(23))
+        D.text(
+            (1623,
+             155),
+            f'{optionmap.get(WeaponSubOPKey) or WeaponSubOPKey}  {str(WeaponSubOPValue)+"%" if WeaponSubOPKey in disper else format(WeaponSubOPValue,",")}',
+            font=config_font(23))
 
     D.rounded_rectangle((1430, 45, 1470, 70), radius=1, fill='black')
     D.text((1433, 46), f'R{WeaponRank}', font=config_font(24))
 
     ScoreLen = D.textlength(f'{ScoreTotal}', config_font(75))
-    D.text((1652-ScoreLen // 2, 420), str(ScoreTotal), font=config_font(75))
+    D.text((1652 - ScoreLen // 2, 420), str(ScoreTotal), font=config_font(75))
     blen = D.textlength(f'{ScoreCVBasis}換算', font=config_font(24))
-    D.text((1867-blen, 585), f'{ScoreCVBasis}換算', font=config_font(24))
+    D.text((1867 - blen, 585), f'{ScoreCVBasis}換算', font=config_font(24))
 
     if ScoreTotal >= 220:
         ScoreEv = Image.open(f'{cwd}/artifactGrades/SS.png')
@@ -419,9 +444,6 @@ def generation(data: dict) -> str:
 
     Base.paste(ScoreEv, (1806, 345), mask=EvMask)
 
-
-
-
     # 聖遺物
     atftype = list()
     for i, parts in enumerate(['flower', "wing", "clock", "cup", "crown"]):
@@ -436,18 +458,18 @@ def generation(data: dict) -> str:
         enhancer = ImageEnhance.Brightness(Preview)
         Preview = enhancer.enhance(0.6)
         Preview = Preview.resize(
-            (int(Preview.width*1.3), int(Preview.height*1.3)))
+            (int(Preview.width * 1.3), int(Preview.height * 1.3)))
         Pmask1 = Preview.copy()
 
         Pmask = Image.open(
             f'{cwd}/Assets/ArtifactMask.png').convert('L').resize(Preview.size)
         Preview.putalpha(Pmask)
         if parts in ['flower', 'crown']:
-            PreviewPaste.paste(Preview, (-37+373*i, 570), mask=Pmask1)
+            PreviewPaste.paste(Preview, (-37 + 373 * i, 570), mask=Pmask1)
         elif parts in ['wing', 'cup']:
-            PreviewPaste.paste(Preview, (-36+373*i, 570), mask=Pmask1)
+            PreviewPaste.paste(Preview, (-36 + 373 * i, 570), mask=Pmask1)
         else:
-            PreviewPaste.paste(Preview, (-35+373*i, 570), mask=Pmask1)
+            PreviewPaste.paste(Preview, (-35 + 373 * i, 570), mask=Pmask1)
         Base = Image.alpha_composite(Base, PreviewPaste)
         D = ImageDraw.Draw(Base)
 
@@ -460,7 +482,14 @@ def generation(data: dict) -> str:
         MainIcon = Image.open(
             f'{cwd}/emotes/{mainop}.png').convert("RGBA").resize((35, 35))
         MainMask = MainIcon.copy()
-        Base.paste(MainIcon, (340+i*373-int(mainoplen), 655), mask=MainMask)
+        Base.paste(
+            MainIcon,
+            (340 +
+             i *
+             373 -
+             int(mainoplen),
+                655),
+            mask=MainMask)
 
         mainv = details['main']['value']
         if mainop in disper:
@@ -474,12 +503,12 @@ def generation(data: dict) -> str:
             )
         else:
             mainvsize = D.textlength(format(mainv, ","), config_font(49))
-            D.text((375 + i*373 - mainvsize, 690),
+            D.text((375 + i * 373 - mainvsize, 690),
                    format(mainv, ","), font=config_font(49))
         levlen = D.textlength(f'+{details["Level"]}', config_font(21))
-        D.rounded_rectangle((373+i*373-int(levlen), 748,
-                            375+i*373, 771), fill='black', radius=2)
-        D.text((374+i*373-levlen, 749),
+        D.rounded_rectangle((373 + i * 373 - int(levlen), 748,
+                            375 + i * 373, 771), fill='black', radius=2)
+        D.text((374 + i * 373 - levlen, 749),
                f'+{details["Level"]}', font=config_font(21))
 
         if details['Level'] == 20 and details['rarelity'] == 5:
@@ -498,37 +527,57 @@ def generation(data: dict) -> str:
             SubOP = sub['option']
             SubVal = sub['value']
             if SubOP in ['HP', '攻撃力', '防御力']:
-                D.text((79+373*i, 811+50*a), optionmap.get(SubOP) or SubOP,
-                       font=config_font(25), fill=(255, 255, 255, 190))
+                D.text((79 + 373 * i, 811 + 50 * a), optionmap.get(SubOP)
+                       or SubOP, font=config_font(25), fill=(255, 255, 255, 190))
             else:
-                D.text((79+373*i, 811+50*a), optionmap.get(SubOP)
+                D.text((79 + 373 * i, 811 + 50 * a), optionmap.get(SubOP)
                        or SubOP, font=config_font(25))
             SubIcon = Image.open(f'{cwd}/emotes/{SubOP}.png').resize((30, 30))
             SubMask = SubIcon.copy()
-            Base.paste(SubIcon, (44+373*i, 811+50*a), mask=SubMask)
+            Base.paste(SubIcon, (44 + 373 * i, 811 + 50 * a), mask=SubMask)
             if SubOP in disper:
                 SubSize = D.textlength(f'{float(SubVal)}%', config_font(25))
-                D.text((375+i*373-SubSize, 811+50*a),
+                D.text((375 + i * 373 - SubSize, 811 + 50 * a),
                        f'{float(SubVal)}%', font=config_font(25))
             else:
                 SubSize = D.textlength(format(SubVal, ","), config_font(25))
                 if SubOP in ['防御力', '攻撃力', 'HP']:
-                    D.text((375+i*373-SubSize, 811+50*a), format(SubVal, ","),
-                           font=config_font(25), fill=(255, 255, 255, 190))
+                    D.text(
+                        (375 + i * 373 - SubSize,
+                         811 + 50 * a),
+                        format(
+                            SubVal,
+                            ","),
+                        font=config_font(25),
+                        fill=(
+                            255,
+                            255,
+                            255,
+                            190))
                 else:
-                    D.text((375+i*373-SubSize, 811+50*a), format(SubVal,
-                           ","), font=config_font(25), fill=(255, 255, 255))
+                    D.text(
+                        (375 + i * 373 - SubSize,
+                         811 + 50 * a),
+                        format(
+                            SubVal,
+                            ","),
+                        font=config_font(25),
+                        fill=(
+                            255,
+                            255,
+                            255))
 
             if details['Level'] == 20 and details['rarelity'] == 5:
                 nobi = D.textlength(
                     "+".join(map(str, psb[a])), font=config_font(11))
-                D.text((375+i*373-nobi, 840+50*a), "+".join(map(str,
+                D.text((375 + i * 373 - nobi, 840 + 50 * a), "+".join(map(str,
                        psb[a])), fill=(255, 255, 255, 160), font=config_font(11))
 
         Score = float(ScoreData[parts])
         ATFScorelen = D.textlength(str(Score), config_font(36))
-        D.text((380+i*373-ATFScorelen, 1016), str(Score), font=config_font(36))
-        D.text((295+i*373-ATFScorelen, 1025), 'Score',
+        D.text((380 + i * 373 - ATFScorelen, 1016),
+               str(Score), font=config_font(36))
+        D.text((295 + i * 373 - ATFScorelen, 1025), 'Score',
                font=config_font(27), fill=(160, 160, 160))
 
         PointRefer = {
@@ -574,17 +623,19 @@ def generation(data: dict) -> str:
             ScoreImage = Image.open(f'{cwd}/artifactGrades/B.png')
 
         ScoreImage = ScoreImage.resize(
-            (ScoreImage.width//11, ScoreImage.height//11))
+            (ScoreImage.width // 11, ScoreImage.height // 11))
         SCMask = ScoreImage.copy()
 
-        Base.paste(ScoreImage, (85+373*i, 1013), mask=SCMask)
+        Base.paste(ScoreImage, (85 + 373 * i, 1013), mask=SCMask)
 
     SetBounus = Counter([x for x in atftype if atftype.count(x) >= 2])
     for i, (n, q) in enumerate(SetBounus.items()):
         if len(SetBounus) == 2:
-            D.text((1536, 243+i*35), n, fill=(0, 255, 0), font=config_font(23))
-            D.rounded_rectangle((1818, 243+i*35, 1862, 266+i*35), 1, 'black')
-            D.text((1835, 243+i*35), str(q), font=config_font(19))
+            D.text((1536, 243 + i * 35), n,
+                   fill=(0, 255, 0), font=config_font(23))
+            D.rounded_rectangle(
+                (1818, 243 + i * 35, 1862, 266 + i * 35), 1, 'black')
+            D.text((1835, 243 + i * 35), str(q), font=config_font(19))
         if len(SetBounus) == 1:
             D.text((1536, 263), n, fill=(0, 255, 0), font=config_font(23))
             D.rounded_rectangle((1818, 263, 1862, 288), 1, 'black')
